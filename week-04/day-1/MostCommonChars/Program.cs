2aa454c8dd6518065b5ec85e484ad0dacbabd314
Filip <filipfinrasek@gmail.com>
2019-09-30 16:57:41 +0200

    added trial exam files

---
+++ week-04/day-1/MostCommonChars/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostCommonChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string fileName = "countchar.txt";
+            string path = @"./countchar.txt";
+
+            //string path = $"./{fileName}";
+            string countcharFile = File.ReadAllText(path.Trim());
+            char[] charsInFile = countcharFile.ToCharArray();
+
+
+            for (int i = 0; i < countcharFile.ToCharArray().Length; i++)
+            {
+
+                Console.Write(charsInFile[i]);
+            }
+
+            foreach (char charInString in countcharFile)
+            {
+
+            }
+
+
+            var count = countcharFile.Count(x => x == 'e'); // copy paste for inspiration reasons .. 
+            Console.WriteLine(count);
+
+
+            Console.ReadLine();
+
+
+        //}
+
+        /*public static int CountChars(string fileName)
+        {
+            string path = $"./{fileName}";
+            string countcharFile = File.ReadAllText(path);
+            char[] charsInFile = countcharFile.ToCharArray();
+
+            int count = 0;
+
+            for (int i = 0; i < countcharFile.ToCharArray().Length; i++)
+            {
+
+                for (int k = 0; k < countcharFile.ToCharArray().Length; k++)
+                {
+                    if (countcharFile.ToCharArray()[i] == countcharFile.ToCharArray()[k])
+                    {
+                        count++;
+                    }
+                    Console.WriteLine($"{countcharFile.ToCharArray()[i]} : {count}");
+                }
+
+                
+            }*/
+
+
+        }
+    }
+}
---
+++ week-04/day-1/isSymmetric/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isSymmetric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] array = new int[3, 3];
+
+            IsSymmetric(array, 3, 3);
+
+
+            Console.ReadLine();
+
+        }
+
+        public static void IsSymmetric(int[,] newArray, int x, int y)
+        {
+            int[,] array = new int[x, y];
+
+            for (int row = 0; row < array.Length / x; row++)
+            {
+
+                for (int column = 0; column < array.Length / y; column++)
+                {
+                    bool sameCoordinates = row == column;
+                    if (sameCoordinates)
+                    {
+                        array[row, column] = 1;
+                    }
+                    Console.Write(array[row, column]);
+                    Console.Write(" ");
+
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
