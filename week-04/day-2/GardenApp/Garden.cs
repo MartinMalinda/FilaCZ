0faabf80d194b81a9069d467110629628e811157
Filip <filipfinrasek@gmail.com>
2019-10-10 16:40:05 +0200

    Added GardenApp task

---
+++ week-04/day-2/GardenApp/Garden.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenApp
+{
+    class Garden
+    {
+        public virtual void Watering(int amountOfWater, List<Plants> plants)
+        {
+
+        }
+    }
+}
---
+++ week-04/day-2/GardenApp/Plants.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenApp
+{
+    public enum PlantType
+    {
+        Tree,
+        Flower
+    }
+    class Plants : Garden
+    {
+        public string Color { get; private set; }
+
+        public bool NeedWater { get; private set; }
+
+        public PlantType TypePlant { get; private set;}
+
+        public Plants(PlantType typePlant, string color, bool needWater = true)
+        {
+            this.Color = color;
+            this.TypePlant = typePlant;
+        }
+
+        public override void Watering(int amountOfWater, List<Plants> plants)
+        {
+            foreach (var plant in plants)
+            {
+                if ()
+                {
+
+                }
+            }
+        }
+    }
+
+    
+}
