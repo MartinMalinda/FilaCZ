367859c6b4abb0f395c08303ed5f24be91d4e4be
Filip <filipfinrasek@gmail.com>
2019-10-10 10:42:48 +0200

    Added Pirates task

---
+++ week-03/day-4/Pirates/Pirate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates
+{
+    class Pirate
+    {
+
+        public int DrinksTaken { get; private set; } = 0;
+        public bool isNotDead { get; private set; } = true;
+
+        public Pirate()
+        {
+            DrinksTaken = DrinksTaken;
+        }
+
+        public void DrinkSomeRum()
+        {
+            if (isNotDead)
+            {
+                DrinksTaken++;
+            }
+            else
+                Console.WriteLine("He's dead.");
+        }
+
+        public void HowsItGoingMate()
+        {
+            if (DrinksTaken <= 4 && isNotDead)
+            {
+                Console.WriteLine("Pour me anudder!");
+            }
+            else if (!isNotDead)
+            {
+                Console.WriteLine("...dead...");
+            }
+            else
+            {
+                Console.WriteLine("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+                DrinksTaken = 0;
+            }
+        }
+
+        public void Brawl(Pirate pirateOne, Pirate pirateTwo)
+        {
+            Random random = new Random();
+            int chance = random.Next(1, 4);
+
+            if (!pirateTwo.isNotDead)
+            {
+                Console.WriteLine("Can't fight dead pirate.");
+                return;  
+            }
+
+
+
+        }
+
+        public bool Die()
+        {
+            return isNotDead = false;
+        }
+
+    }
+}
---
+++ week-03/day-4/Pirates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pirate jack = new Pirate();
+
+            jack.DrinkSomeRum();
+            jack.DrinkSomeRum();
+            jack.DrinkSomeRum();
+            jack.DrinkSomeRum();
+            jack.DrinkSomeRum();
+
+            jack.HowsItGoingMate();
+
+            jack.HowsItGoingMate();
+
+            jack.Die();
+
+            jack.DrinkSomeRum();
+
+            jack.HowsItGoingMate();
+
+
+            Console.ReadKey();
+        }
+    }
+}
