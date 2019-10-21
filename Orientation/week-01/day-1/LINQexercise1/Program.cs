a55b0a3fa20b67a110743d31ab0743e0fd44d591
Filip <filipfinrasek@gmail.com>
2019-10-21 11:50:55 +0200

    Added LINQ excercise

---
+++ Orientation/week-01/day-1/LINQexercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQexercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var evenNumsMethod = numbers.Where(n => n % 2 == 0);
+
+            var evenNums = from number in numbers
+                           where number % 2 == 0
+                           select number;
+
+            foreach (var item in evenNums)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
