bc93f7584b3270683057c1923cc5a5423d1612e1
Filip <filipfinrasek@gmail.com>
2019-10-21 15:18:27 +0200

    Added LINQexercise5

---
+++ Orientation/week-01/day-1/LINQexercise5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQexercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var frequencyOfNumsMethod = numbers.GroupBy(num => num);
+            frequencyOfNumsMethod.ToList().ForEach(num => Console.WriteLine(num.Key + " " + num.Count()));
+
+            /*foreach (var num in frequencyOfNums)
+            {
+
+                foreach (var item in num)
+                {
+                    Console.WriteLine(item);
+                }
+           
+            }*/
+
+            Console.WriteLine();
+
+            var frequencyOfNums = from number in numbers
+                                  group numbers.GroupBy(num => num)
+                                  by number;
+
+            frequencyOfNums.ToList().ForEach(num => Console.WriteLine(num.Key + " " + num.Count()));
+
+            Console.ReadLine();
+        }
+    }
+}
