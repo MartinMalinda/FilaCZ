7ec5a256696e66e235fc63402b881da3edf0452a
Filip <filipfinrasek@gmail.com>
2019-10-21 16:17:05 +0200

    Added LINQexercise7

---
+++ Orientation/week-01/day-1/LINQexercise7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQexercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var startsAEndsIMethod = cities.Where(c => c.StartsWith("A") && c.EndsWith("I"));
+            startsAEndsIMethod.ToList().ForEach(c => Console.WriteLine(c));
+
+            Console.WriteLine();
+
+            var startsAEndsI = from c in cities
+                               where c.StartsWith("A") && c.EndsWith("I")
+                               select c;
+
+            startsAEndsI.ToList().ForEach(c => Console.WriteLine(c));
+
+            Console.ReadLine();
+        }
+    }
+}
