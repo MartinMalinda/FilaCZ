c36d3579751a6132959f6576801d107ecb8b3338
Filip <filipfinrasek@gmail.com>
2019-10-21 17:04:10 +0200

    Added LINQexercise9

---
+++ Orientation/week-01/day-1/LINQexercise9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQexercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = { 'a', 'h', 'o', 'j' };
+            var word = String.Join("", chars.Select(c => c));
+            Console.WriteLine(word);
+
+
+            Console.ReadLine();
+        }
+    }
+}
