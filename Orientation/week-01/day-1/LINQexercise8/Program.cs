391c61d8718c92aabc548786e99a85c2225ac1e1
Filip <filipfinrasek@gmail.com>
2019-10-21 16:33:54 +0200

    Added LINQexercise8

---
+++ Orientation/week-01/day-1/LINQexercise8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQexercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "NasHleDanoU";
+
+            var upperCaseCharsMethod = word.Where(c => Char.IsUpper(c));
+            upperCaseCharsMethod.ToList().ForEach(c => Console.WriteLine(c));
+
+            Console.WriteLine();
+
+            var upperCaseChars = from c in word
+                                 where Char.IsUpper(c)
+                                 select c;
+
+            upperCaseChars.ToList().ForEach(c => Console.WriteLine(c));
+
+            Console.ReadLine();
+        }
+    }
+}
