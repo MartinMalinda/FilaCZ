06627a1154b90a5d861d1fa373b36349f2c2cf41
Filip <filipfinrasek@gmail.com>
2019-10-21 14:33:58 +0200

    Added LINQexercise4

---
+++ Orientation/week-01/day-1/LINQexercise4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQexercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var biggerThanTwentyMethod = numbers.Where(n => n * n > 20);
+            biggerThanTwentyMethod.ToList().ForEach(n => Console.WriteLine(n));
+
+            var biggerThanTwenty = from number in numbers
+                                   where number * number > 20
+                                   select number;
+
+            biggerThanTwenty.ToList().ForEach(number => Console.WriteLine(number));
+
+            Console.ReadLine();
+        }
+    }
+}
