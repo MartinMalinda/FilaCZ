5d52d508c8e76b4d8d3c39ab9740aab883a5961a
Filip <filipfinrasek@gmail.com>
2019-10-21 17:36:22 +0200

    Added LINQfoxes task

---
+++ Orientation/week-01/day-1/LINQfox/Fox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQfox
+{
+    public enum FoxType
+    {
+        pallida,
+        chama,
+        arctic
+
+    }
+    public class Fox
+    {
+        public string Name { get; private set; }
+        public FoxType Type { get; private set; }
+
+        public string Color { get; private set; }
+
+        public Fox(string name, FoxType type, string color)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+        }
+           
+
+    }
+}
---
+++ Orientation/week-01/day-1/LINQfox/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQfox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fox> foxes = new List<Fox>();
+            foxes.Add(new Fox("Karel", FoxType.pallida, "green"));
+            foxes.Add(new Fox("Josef", FoxType.arctic, "red"));
+            foxes.Add(new Fox("Frantisek", FoxType.pallida, "green"));
+            foxes.Add(new Fox("Alfred", FoxType.chama, "violet"));
+            foxes.Add(new Fox("Boromir", FoxType.arctic, "green"));
+
+            var greenFox = foxes.Where(f => f.Color == "green");
+
+            greenFox.ToList().ForEach(f => Console.WriteLine(f.Name));
+
+            var greenPallida = foxes.Where(f => f.Color == "green" && f.Type == FoxType.pallida);
+
+            greenPallida.ToList().ForEach(f => Console.WriteLine(f.Name));
+
+            Console.ReadLine();
+        }
+    }
+}
