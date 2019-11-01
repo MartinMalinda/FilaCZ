955b00007e08b08d5eeb738a0e0b7b625d81db8a
Filip <filipfinrasek@gmail.com>
2019-11-01 15:23:19 +0100

    Added Fox Club task

---
+++ Orientation/week-02/day-5/FoxClub/FoxClub/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoxClub.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoxClub.Controllers
+{
+    public class HomeController : Controller
+    {
+        //List<Models.Fox> foxes = new List<Models.Fox>();
+
+        private FoxService FoxService { get; set; }
+
+        public HomeController(FoxService foxService)
+        {
+            this.FoxService = foxService;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("login")]
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(string name)
+        {
+            FoxService.AddFoxToList(name);
+            return RedirectToAction("information", "home", new { Name = name }); //cant redirect to HttpPost!!!! Use Route
+        }
+
+        [Route("information")] //Route because of redirect
+        public IActionResult Information(string name)
+        {
+            var fox = FoxService.GetFox(name);
+            return View("information", fox);
+        }
+    }
+}
